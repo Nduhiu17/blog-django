3ab7868419ad376c481ba7fb45995300f7485cdb
nduhiu17 <nduhiu254@gmail.com>
2019-01-17 08:20:30 +0300

    create method to create an article

--- article/views.py
+++ article/views.py
@@ -4,9 +4,20 @@ from rest_framework.views import APIView
 from .models import Article
 from .serializers import ArticleSerializer
 
+
 class ArticleView(APIView):
     def get(self, request):
         articles = Article.objects.all()
         # the many param informs the serializer that it will be serializing more than a single article.
         serializer = ArticleSerializer(articles, many=True)
         return Response({"articles": serializer.data})
+
+    def post(self, request):
+        article = request.data.get('article')
+
+        # Create an article from the above data
+        serializer = ArticleSerializer(data=article,partial=True)
+        if serializer.is_valid(raise_exception=True):
+            article_saved = serializer.save()
+
+        return Response({"success": "Article '{}' created successfully".format(article_saved.title)})
